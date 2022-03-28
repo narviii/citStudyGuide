47b64a514af106fe552416616293f3df1f74fc1f
Vladimir Sofronov <narviii@gmail.com>
2022-03-27 22:52:34 -0700

    added few more docs

---
+++ src/data/impDatesEvents.js
@@ -0,0 +1,63 @@
+export const impDatesEvents=[
+    {
+        title:'January 1',
+        cardDetailedText:'New Year’s Day '
+    },
+    {
+        title:'January 11',
+        cardDetailedText:'Sir John A. Macdonald Day',
+    },
+    {
+        title:'Friday, preceding Easter Sunday',
+        cardDetailedText:'Good Friday.'
+    },
+    {
+        title:'Monday following Easter Sunday',
+        cardDetailedText:'Easter Monday'
+    },
+    {
+        title:'April 9',
+        cardDetailedText:'Vimy Day.'
+    },
+    {
+        title:'Monday preceding May 25',
+        cardDetailedText:'Victoria Day'
+    },
+    {
+        title:'May 25',
+        cardDetailedText:'Sovereign’s birthday'
+    },
+    {
+        title:'June 24',
+        subTitle:'Quebec',
+        cardDetailedText:'Feast of St. John the Baptist.',
+    },
+    {
+        title:'July 1',
+        cardDetailedText:'Canada Day.'
+    },
+    {
+        title:'First Monday of September',
+        cardDetailedText:'Labour Day.'
+    },
+    {
+        title:'Second Monday of October',
+        cardDetailedText:'Thanksgiving Day.'
+    },
+    {
+        title:'November 11',
+        cardDetailedText:'Remembrance Day. Canadians wear the red poppy and observe a moment of silence at the 11th hour of the 11th day of the 11th month to honour the sacrifices of over a million people who have served, and the 110,000 who have given their lives in all wars in which Canadians took part.'
+    },
+    {
+        title:'November 20',
+        cardDetailedText:'Sir Wilfrid Laurier Day.'
+    },
+    {
+        title:'December 25',
+        cardDetailedText:'Christmas Day.'
+    },
+    {
+        title:'December 26',
+        cardDetailedText:'Boxing day.'
+    }
+]
